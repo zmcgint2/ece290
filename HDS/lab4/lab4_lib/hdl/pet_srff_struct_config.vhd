c9f9924f52f57e5831107c6c5d7561df44cd1c04
zmcginty <zmcginty@gmail.com>
2013-10-09 15:48:15 -0500

    added HDS and lab4 library

---
+++ HDS/lab4/lab4_lib/hdl/pet_srff_struct_config.vhd
@@ -0,0 +1,11 @@
+-- Generation properties:
+--   Format              : hierarchical
+--   Generic mappings    : exclude
+--   Leaf-level entities : direct binding
+--   Regular libraries   : use library name
+--   View name           : include
+--   
+CONFIGURATION pet_srff_struct_config OF pet_srff IS
+   FOR struct
+   END FOR;
+END pet_srff_struct_config;
---
+++ HDS/lab4/lab4_lib/hdl/sr_latch_struct_config.vhd
@@ -0,0 +1,11 @@
+-- Generation properties:
+--   Format              : hierarchical
+--   Generic mappings    : exclude
+--   Leaf-level entities : direct binding
+--   Regular libraries   : use library name
+--   View name           : include
+--   
+CONFIGURATION sr_latch_struct_config OF sr_latch IS
+   FOR struct
+   END FOR;
+END sr_latch_struct_config;
