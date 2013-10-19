1052c75ae50fd92c5b31ce03f49a258a618d7615
zmcginty <zmcginty@gmail.com>
2013-10-18 22:36:51 -0500

    instead of storing each letter of the alphabet in registers, i did the same thing with muxes and 5-bit a-z and just added 97 to shift it to its' ascii value. addressing should be the same as before.

--- project1/project1_lib/hdl/alphabetsoup_struct_config.vhd
+++ project1/project1_lib/hdl/alphabetsoup_struct_config.vhd
@@ -5,7 +5,11 @@
 --   Regular libraries   : use library name
 --   View name           : include
 --   
+LIBRARY project1_lib;
 CONFIGURATION alphabetsoup_struct_config OF alphabetsoup IS
    FOR struct
+      FOR ALL : add_97_alphabet_to_ascii
+         USE CONFIGURATION project1_lib.add_97_alphabet_to_ascii_struct_config;
+      END FOR;
    END FOR;
 END alphabetsoup_struct_config;
