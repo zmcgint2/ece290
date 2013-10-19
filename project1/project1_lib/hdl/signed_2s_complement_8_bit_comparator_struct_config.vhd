87f075610275e39cd69b9e296b7e9e094cd4a49c
Harrison Kiang <hkiang2@gelib-057-06.ews.illinois.edu>
2013-10-19 18:26:15 -0500

    function_cipher files added, not variable yet.

---
+++ project1/project1_lib/hdl/signed_2s_complement_8_bit_comparator_struct_config.vhd
@@ -0,0 +1,4 @@
+CONFIGURATION signed_2s_complement_8_bit_comparator_struct_config OF signed_2s_complement_8_bit_comparator IS
+   FOR struct
+   END FOR;
+END signed_2s_complement_8_bit_comparator_struct_config;
