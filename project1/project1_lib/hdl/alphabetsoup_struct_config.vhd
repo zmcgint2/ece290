64cc5ca3ee1a8085cd5ea540e0c30e3ec3b6eee1
zmcginty <zmcginty@gmail.com>
2013-10-18 21:21:50 -0500

    ascii to dec(1-26)alphabet converters done. not tested. giant mux for 5 bit (1-26) alphabet with code signals sent in then add 97 to output ascii alphabet

---
+++ project1/project1_lib/hdl/alphabetsoup_struct_config.vhd
@@ -0,0 +1,11 @@
+-- Generation properties:
+--   Format              : hierarchical
+--   Generic mappings    : exclude
+--   Leaf-level entities : direct binding
+--   Regular libraries   : use library name
+--   View name           : include
+--   
+CONFIGURATION alphabetsoup_struct_config OF alphabetsoup IS
+   FOR struct
+   END FOR;
+END alphabetsoup_struct_config;
