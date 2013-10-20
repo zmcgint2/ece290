5b15ec41cee7b3f6c3f2eaf99887eccf16b2e43f
Jimmy Chen <jschen3@evrt-252-17.ews.illinois.edu>
2013-10-20 17:43:05 -0500

    updated my ceasar shifts should be functional. For some reason simulating may fail

---
+++ project1/project1_lib/hdl/jceasarshiftdecode_struct_config.vhd
@@ -0,0 +1,21 @@
+-- Generation properties:
+--   Format              : hierarchical
+--   Generic mappings    : exclude
+--   Leaf-level entities : direct binding
+--   Regular libraries   : use library name
+--   View name           : include
+--   
+LIBRARY project1_lib;
+CONFIGURATION jceasarshiftdecode_struct_config OF jceasarshiftdecode IS
+   FOR struct
+      FOR ALL : full_adder_8bit
+         USE CONFIGURATION project1_lib.full_adder_8bit_struct_config;
+      END FOR;
+      FOR ALL : j21mux
+         USE CONFIGURATION project1_lib.j21mux_struct_config;
+      END FOR;
+      FOR ALL : j7bitcomparator
+         USE CONFIGURATION project1_lib.j7bitcomparator_struct_config;
+      END FOR;
+   END FOR;
+END jceasarshiftdecode_struct_config;
