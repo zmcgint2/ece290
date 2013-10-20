d2821b5aca5c028c61f12d10ee64231b86616ce6
Chirag Patil <cjpatil2@evrt-252-18.ews.illinois.edu>
2013-10-20 17:16:21 -0500

    lets see if THIS method of adding files works...

---
+++ project1/project1_lib/hdl/dflipflop_struct_config.vhd
@@ -0,0 +1,11 @@
+-- Generation properties:
+--   Format              : hierarchical
+--   Generic mappings    : exclude
+--   Leaf-level entities : direct binding
+--   Regular libraries   : use library name
+--   View name           : include
+--   
+CONFIGURATION dflipflop_struct_config OF dflipflop IS
+   FOR struct
+   END FOR;
+END dflipflop_struct_config;
---
+++ project1/project1_lib/hdl/twotobin_struct_config.vhd
@@ -0,0 +1,11 @@
+-- Generation properties:
+--   Format              : hierarchical
+--   Generic mappings    : exclude
+--   Leaf-level entities : direct binding
+--   Regular libraries   : use library name
+--   View name           : include
+--   
+CONFIGURATION twotobin_struct_config OF twotobin IS
+   FOR struct
+   END FOR;
+END twotobin_struct_config;
